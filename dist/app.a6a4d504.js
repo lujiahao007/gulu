cf3f5e4d9e924d60eaf5d4b08e0969984f4f8a85
“lujiahao007” <1023717591@qq.com>
2022-10-21 11:09:34 +0800

    测试持续集成

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -12729,7 +12729,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51218" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64580" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
